8693c7945c54dbebb33ced72e00f96f96ff5d8b8
Tyler694 <141347012+Tyler694@users.noreply.github.com>
2023-12-18 19:52:23 +0000

    Tomatoes and containers
    added tomato veg and its container

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ clock = pygame.time.Clock()
 
 ItalianBread6InchImage = pygame.image.load("img/Bread/Italian 6-inch.png")
 FloorImage = pygame.image.load("img/Background/floor.png")
+TomatoesImage = pygame.image.load("img/Vegatables/tomatoes.png")
+TomatoesContainer = pygame.image.load("img/Container/tomatoesContainer.png")
+
 
 subX = 800
 
@@ -16,12 +19,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    if(pygame.mouse.get_pos() <= (606, 175) and pygame.mouse.get_pos() >= (606+194, 175+258)):
+        print("Colliding")
+
     screen.blit(FloorImage, (0,0))
     screen.blit(ItalianBread6InchImage, (subX, 450))
+    screen.blit(TomatoesImage, (subX, 450))
+    screen.blit(TomatoesContainer, (606, 175))
 
     subX-=2
 
-    print(subX)
-
     clock.tick(60)
+    pygame.display.flip()
     pygame.display.update()
